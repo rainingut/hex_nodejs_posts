f5a95824c54330882e3029bfb7dfbaa4ca15ccb5
rainingut <keeprainingut@gmail.com>
2022-05-03 11:42:17 +0800

    修改controller 錯誤訊息、阻擋空白內容與標題；修改models增加預設

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,17 @@ const posts = {
 
   async postPost(request, response){
     try {
-      const data = await buffleHandler(request);
+      const data = (await buffleHandler(request));
+      if (!data.name || !data.name.trim()) {
+        errorHandler({response, statusCode: 400, content: { message: '標題必填'}});
+        return;
+      }
+      if (!data.content || !data.content.trim()) {
+        errorHandler({response, statusCode: 400, content: { message: '內容必填'}});
+        return;
+      }
+      data.name = data.name.trim();
+      data.content = data.content.trim();
       PostModel.postDB(data)
         .then(async(result) => 
           successHandler(response, {message: '新增成功', data: await PostModel.getDB()}))
@@ -36,14 +46,27 @@ const posts = {
 
   async patchPost(request,response){
     const id = request.url.split('/').pop();
-    const isexist = await PostModel.existsDB(id);
     try {
-      if (!isexist) {
-        errorHandler(  {response, statusCode:400, content:{message:'查無此ID'}}  );
+      const isexist = await PostModel.existsDB(id)
+      if(!isexist) {
+        errorHandler({response, statusCode: 400, content: { message: '查無此項' }});
         return;
       }
-
       const data = await buffleHandler(request);
+      if (data.name !== undefined) {
+        data.name = data.name.trim();
+        if( !data.name ) {
+          errorHandler({response, statusCode: 400, content: { message: '標題必填'}});
+          return;
+        }
+      }
+      if (data.content !== undefined) {
+        data.content = data.content.trim();
+        if (!data.content){
+          errorHandler({response, statusCode: 400, content: { message: '內容必填'}});
+          return;
+        }
+      }
       PostModel.patchDB(id, data)
         .then(async(result) => {
           const posts = await PostModel.getDB()
@@ -54,26 +77,31 @@ const posts = {
         });
     }
     catch(error){
-      errorHandler({response, statusCode: 400, content: { message: '格式錯誤', error }});
+      errorHandler({response, statusCode: 400, content: { message: '格式錯誤或查無此項' }});
     }
   },
 
 
   async deletePost(request,response){
     const id = request.url.split('/').pop();
-    const isexist = await PostModel.existsDB(id)
-    if(!isexist) {
-      errorHandler(  {response, statusCode:400, content:{message:'查無此ID'}}  );
-      return;
+    try{
+      const isexist = await PostModel.existsDB(id);
+      if (!isexist){
+        errorHandler(  {response, statusCode:400, content:{message:'查無此項'}}  );
+        return;
+      }
+      PostModel.deleteOneDB(id)
+        .then(async(result) => {
+          const posts = await PostModel.getDB()
+          successHandler(response, {data: posts, message: '刪除成功'})
+        })
+        .catch(error => {
+          errorHandler({response, statusCode:400, content:{message:error}}); 
+        });
     }
-    PostModel.deleteOneDB(id)
-      .then(async(result) => {
-        const posts = await PostModel.getDB()
-        successHandler(response, {data: posts, message: '刪除成功'})
-      })
-      .catch(error => {
-        errorHandler({response, statusCode:400, content:{message:error}}); 
-      });
+    catch(error){
+      errorHandler(  {response, statusCode:400, content:{message:'格式錯誤或查無此項'}}  );
+    }
   },
 
 
